60cf6b959db7b6cff829ff61e77d03158a2ff88d
ShUl0w <37832993+ShUl0w@users.noreply.github.com>
2023-06-13 14:19:05 +0000

    up and running for real

---
+++ clientapp/app/components/board/board.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="board">
+        <div class="board-x">
+            <div class="board-y" v-for="xrow in data.boardData">
+                <tile v-for="field in xrow" :fieldData="field"></tile>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+import tile from "@/app/components/board/board-tile.vue"
+
+const data = defineProps({
+    boardData: {
+        type: Array,
+        default: () => []
+    }
+})
+
+</script>
+
+<style lang="scss">
+.board {
+    display: flex;
+    flex-direction: row;
+    flex: 0 1 auto;
+    aspect-ratio: 1 / 1;
+
+    .board-x {
+        display: flex;
+        flex-direction: row;
+        flex: 1 1 auto;
+        gap: 0.25em;
+
+        .board-y {
+            display: flex;
+            flex-direction: column;
+            flex: 1 1 auto;
+            gap: 0.25em;
+        }
+    }
+}
+</style>
