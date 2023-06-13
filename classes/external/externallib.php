40100352296ea7771351f12dc6f21bc42f230648
Lars Bonczek <bonczek@tu-berlin.de>
2023-06-13 17:31:40 +0200

    Update external services

--- classes/external/externallib.php
+++ classes/external/externallib.php
@@ -6,6 +6,8 @@ use external_function_parameters;
 use external_multiple_structure;
 use external_value;
 use external_single_structure;
+use mod_gameofknowledge\game_exception;
+use mod_gameofknowledge\game_manager;
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -17,7 +19,7 @@ class externallib extends \external_api {
         ]);
     }
 
-    public static function perform_move_parameters() {
+    public static function perform_action_parameters() {
         return new external_function_parameters([
             'coursemoduleid' => new external_value(PARAM_INT, 'course module id'),
             'action' => new external_value(PARAM_RAW, 'action to be done')
@@ -28,7 +30,7 @@ class externallib extends \external_api {
         return new external_value(PARAM_RAW, 'response');
     }
 
-    public static function perform_move_returns() {
+    public static function perform_action_returns() {
         return new external_value(PARAM_RAW, 'response');
     }
 
@@ -38,37 +40,50 @@ class externallib extends \external_api {
      * @throws \invalid_parameter_exception
      */
     public static function get_state($coursemoduleid) {
-        $params = self::validate_parameters(self::get_rooms_parameters(), ['coursemoduleid' => $coursemoduleid]);
+        $params = self::validate_parameters(self::get_state_parameters(), ['coursemoduleid' => $coursemoduleid]);
 
         list($course, $coursemodule) = get_course_and_cm_from_cmid($params['coursemoduleid'], 'gameofknowledge');
         self::validate_context($coursemodule->context);
 
-        global $PAGE, $DB;
-        $renderer = $PAGE->get_renderer('core');
-        $ctx = $coursemodule->context;
+        $manager = new game_manager($coursemodule);
 
-        $list = [];
-        $rooms = $DB->get_records('gameofknowledge_rooms', ['gameofknowledgeid' => $coursemodule->instance]);
-        foreach ($rooms as $room) {
-            $exporter = new exporter\room($room, $ctx);
-            $list[] = $exporter->export($renderer);
+        $player = $manager->get_current_player_record();
+        if (!$player) {
+            throw new game_exception('notingame');
         }
 
-        return $list;
+        $game = $manager->get_game($player->gameid);
+
+        $state = $game->get_player_state($player->number);
+
+        return json_encode($state, JSON_THROW_ON_ERROR);
     }
 
     /**
      * @param $coursemoduleid
      * @param $action
-     * @return void
-     * @throws \core_external\restricted_context_exception
      * @throws \invalid_parameter_exception
      * @throws \moodle_exception
      */
-    public static function perform_move($coursemoduleid, $action) {
-        $params = self::validate_parameters(self::get_rooms_parameters(), ['coursemoduleid' => $coursemoduleid, 'action' => $action]);
+    public static function perform_action($coursemoduleid, $action) {
+        $params = self::validate_parameters(self::perform_action_parameters(), ['coursemoduleid' => $coursemoduleid, 'action' => $action]);
 
         list($course, $coursemodule) = get_course_and_cm_from_cmid($params['coursemoduleid'], 'gameofknowledge');
         self::validate_context($coursemodule->context);
+
+        $manager = new game_manager($coursemodule);
+
+        $player = $manager->get_current_player_record();
+        if (!$player) {
+            throw new game_exception('notingame');
+        }
+
+        $game = $manager->get_game($player->gameid);
+
+        $game->perform_action($player->number, $params['action']);
+
+        $state = $game->get_player_state($player->number);
+
+        return json_encode($state, JSON_THROW_ON_ERROR);
     }
 }
--- db/services.php
+++ db/services.php
@@ -1,17 +1,17 @@
 <?php
 
 $functions = array(
-    'mod_gameofknowledge_get_rooms' => array(
+    'mod_gameofknowledge_get_state' => array(
         'classname' => 'mod_gameofknowledge\external\externallib',
         'methodname' => 'get_state',
         'description' => 'Get the current game state.',
         'type' => 'read',
         'ajax' => true,
     ),
-    'mod_gameofknowledge_get_rooms' => array(
+    'mod_gameofknowledge_perform_action' => array(
         'classname' => 'mod_gameofknowledge\external\externallib',
         'methodname' => 'perform_action',
-        'description' => 'Input a new move.',
+        'description' => 'Input a new action.',
         'type' => 'write',
         'ajax' => true,
     )
