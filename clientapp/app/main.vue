82f00ec8237eff53d257a4c3e89936d95bdc4539
ShUl0w <37832993+ShUl0w@users.noreply.github.com>
2023-06-13 14:55:58 +0000

    working movement

--- clientapp/app/main.vue
+++ clientapp/app/main.vue
@@ -1,17 +1,24 @@
 <template>
     <div class="mod-gameofknowledge">
-        <board :boardData="boardState.boardData"></board>
+        <board :boardData="boardState.boardData" :playerState="playerState"></board>
     </div>
 </template>
 
 <script setup>
-import { ref, reactive, inject, onMounted } from "vue";
+import { ref, reactive, onMounted } from "vue";
 import board from "@/app/components/board/board.vue"
+
+const playerState = reactive({
+    currentPosition: {
+        posX: 0,
+        posY: 0
+    }
+})
 
 const boardState = reactive({
     boardData: [
-        [{ question: "Question", type: 1 }, { question: "Question", type: 1 }, { question: "Question", type: 1 }],
-        [{ question: "Question", type: 4 }, { question: "Question", type: 1 }, { question: "Question", type: 1 }],
+        [{ question: "Question", type: 1 }, { question: "Question", type: 1 }, { question: "Question", type: 2 }],
+        [{ question: "Question", type: 4 }, { question: "Question", type: 5 }, { question: "Question", type: 1 }],
         [{ question: "Question", type: 1 }, { question: "Question", type: 4 }, { question: "Question", type: 1 }],
         [{ question: "Question", type: 1 }, { question: "Question", type: 1 }, { question: "Question", type: 4 }]]
 })
