16ace7ba393d38e51935e615d1447a7d0d27fcf1
ShUl0w <37832993+ShUl0w@users.noreply.github.com>
2023-06-14 07:26:09 +0000

    added vuex and externals

--- clientapp/index.js
+++ clientapp/index.js
@@ -1,8 +1,10 @@
 import { createApp } from "vue";
 import App from "@/app/main";
+import Store from "@/app/store";
 
 const init = () => {
   const app = createApp(App);
+  app.use(Store);
   app.mount("#mod-gameofknowledge-app");
 };
 
