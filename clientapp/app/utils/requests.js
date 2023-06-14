7ee9b2b0b2ee6ad2530ab4c1e56557a5a3c7436d
ShUl0w <37832993+ShUl0w@users.noreply.github.com>
2023-06-14 08:27:55 +0000

    implemented api calls

--- clientapp/app/utils/requests.js
+++ clientapp/app/utils/requests.js
@@ -1,7 +1,79 @@
 import { call as fetchMany } from "core/ajax";
 
-const requestGameStart = async (coursemoduleid) => {
-    
+const REQUEST_BASE = "mod_gameofknowledge";
+const REQUEST_TEST = `${REQUEST_BASE}_test`;
+const REQUEST_START_GAME = `${REQUEST_BASE}_start_game`;
+const REQUEST_GET_STATE = `${REQUEST_BASE}_get_state`;
+const REQUEST_PERFORM_ACTION = `${REQUEST_BASE}_perform_action`;
+
+const requestTest = async (coursemoduleid, args = null) => {
+  const request = {
+    methodname: REQUEST_TEST,
+    args: Object.assign(
+      {
+        coursemoduleid: coursemoduleid,
+      },
+      args
+    ),
+  };
+
+  try {
+    return await fetchMany([request])[0];
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-export { requestGameStart };
+const requestStartGame = async (coursemoduleid, args = null) => {
+  const request = {
+    methodname: "",
+    args: Object.assign(
+      {
+        coursemoduleid: coursemoduleid,
+      },
+      args
+    ),
+  };
+
+  try {
+    return await fetchMany([request])[0];
+  } catch (e) {
+    console.log(e);
+  }
+};
+const requestGetState = async (coursemoduleid, args = null) => {
+  const request = {
+    methodname: "",
+    args: Object.assign(
+      {
+        coursemoduleid: coursemoduleid,
+      },
+      args
+    ),
+  };
+
+  try {
+    return await fetchMany([request])[0];
+  } catch (e) {
+    console.log(e);
+  }
+};
+const requestPerformAction = async (coursemoduleid, args = null) => {
+  const request = {
+    methodname: "",
+    args: Object.assign(
+      {
+        coursemoduleid: coursemoduleid,
+      },
+      args
+    ),
+  };
+
+  try {
+    return await fetchMany([request])[0];
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export { requestTest, requestStartGame, requestGetState, requestPerformAction };
