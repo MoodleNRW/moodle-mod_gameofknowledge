5f9820330a1b439b92fc2c74e3dda9a0641370b4
ShUl0w <37832993+ShUl0w@users.noreply.github.com>
2023-06-14 10:46:01 +0000

    current state

---
+++ clientapp/app/components/question/question.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="question">
+        <h3>Question</h3>
+        <div class="content"></div>
+    </div>
+</template>
+<script setup></script>
+<style lang="scss">
+.question {
+    flex: 0  1 auto;
+    display: flex;
+    flex-direction: column;
+    background-color: white;
+    padding: 1rem;
+    border-radius: 0.25rem;
+
+    .content {}
+}
+</style>
